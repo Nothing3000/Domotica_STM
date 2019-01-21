c47abf598a38831e9f71b40ed791e8b2e15ea964
Unknown <b0776@msn.com>
2019-01-21 19:48:18 +0100

    Button now stores it's data in flash memory (untested)

--- Src/buttonControl.c
+++ Src/buttonControl.c
@@ -9,8 +9,10 @@
 #include "wpan/aps.h"
 #include "xbee/byteorder.h"
 
-wpan_envelope_t buttonEnvelope;
-static uint8_t payload = 0x03;
+static wpan_envelope_t buttonEnvelope;
+static buttonData_t buttonData;
+static const uint8_t payload = 0x03;
+__attribute__((__section__(".user_data"),aligned(4))) static const uint8_t userData[1024];
 
 
 static void sendButtonMessage()
@@ -18,19 +20,63 @@ static void sendButtonMessage()
 	wpan_envelope_send(&buttonEnvelope);
 }
 
-void setButtonEnvelope(uint16_t network_addr,uint8_t dest_endpoint)
+static void setButtonEnvelope()
 {
-	wpan_envelope_create(&buttonEnvelope,&my_xbee.wpan_dev,WPAN_IEEE_ADDR_UNDEFINED,network_addr);
-	buttonEnvelope.dest_endpoint = dest_endpoint;
+
+	wpan_envelope_create(&buttonEnvelope,&my_xbee.wpan_dev,WPAN_IEEE_ADDR_UNDEFINED,buttonData.network_addr);
+	buttonEnvelope.dest_endpoint = buttonData.dest_endpoint;
 	buttonEnvelope.source_endpoint = 0xE8;
 	buttonEnvelope.profile_id = WPAN_PROFILE_DIGI;
 	buttonEnvelope.payload = (void *)&payload;
 	buttonEnvelope.length = 1;
 }
 
+static void saveButtonData()
+{
+	const uint8_t *buttonDataBytes;
+	uint8_t dataBytes[4];
+	uint32_t *data = (uint32_t *)dataBytes;
+	uint32_t address = (uint32_t)&userData[0];
+	uint32_t pageError;
+
+	FLASH_EraseInitTypeDef EraseInit =
+	{
+		FLASH_TYPEERASE_PAGES,
+		0x0800FC00,
+		1
+	};
+
+	buttonDataBytes = (uint8_t *)&buttonData;
+
+	dataBytes[0] = buttonDataBytes[0];
+	dataBytes[1] = buttonDataBytes[1];
+	dataBytes[2] = buttonDataBytes[2];
+	dataBytes[3] = 0xFF;
+
+	HAL_FLASH_Unlock();
+
+	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_WRPERR | FLASH_FLAG_PGERR);
+
+	HAL_FLASHEx_Erase(&EraseInit, &pageError);
+
+	HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, address, *data);
+
+	HAL_FLASH_Lock();
+
+}
+
+static void loadButtonData()
+{
+	buttonData_t *readButtonData;
+	readButtonData = (buttonData_t *)&userData[0];
+
+	buttonData.network_addr=readButtonData->network_addr;
+	buttonData.dest_endpoint=readButtonData->dest_endpoint;
+}
+
 int buttonEndpoint(const wpan_envelope_t *envelope, struct wpan_ep_state_t *ep_state)
 {
-	const buttonData_t *buttonData;
+	const buttonData_t *received;
 	uint16_t length = envelope->length;
 
 	if(length != sizeof(buttonData_t))
@@ -38,9 +84,12 @@ int buttonEndpoint(const wpan_envelope_t *envelope, struct wpan_ep_state_t *ep_s
 		return -1;
 	}
 
-	buttonData = envelope->payload;
+	received = envelope->payload;
+	buttonData.network_addr = be16toh(received->network_addr);
+	buttonData.dest_endpoint = received->dest_endpoint;
 
-	setButtonEnvelope(be16toh(buttonData->network_addr),buttonData->dest_endpoint);
+	saveButtonData(&buttonData);
+	setButtonEnvelope(&buttonData);
 	return 0;
 }
 
@@ -48,7 +97,7 @@ void pollButonTask(void * pvParameters)
 {
 	GPIO_PinState currentState = HAL_GPIO_ReadPin(B2_GPIO_Port, B2_Pin);
 	GPIO_PinState previousState = currentState;
-
+	loadButtonData();
 	for(;;)
 	{
 		currentState = HAL_GPIO_ReadPin(B2_GPIO_Port, B2_Pin);
